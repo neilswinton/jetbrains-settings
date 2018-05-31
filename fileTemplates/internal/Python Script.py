8aab4a44666777d88bb88f6334542b7fe8b6feb3
Neil Swinton <neil@pineknollconsulting.com>
2018-05-31 12:21:18 -0400

    PC-2018.1.3 <nswin@DESKTOP-RBRE8SF Create ide.general.xml, git.xml, Default.xml, project.default.xml, diff.xml, laf.xml, other.xml, jdk.table.xml, Default.xml, find.xml, ui.lnf.xml, filetypes.xml, Python Script.py, remote-servers.xml, editor.codeinsight.xml, plugin_ui.xml, External Tools.xml, editor.xml, usageView.xml, debugger.xml

---
+++ fileTemplates/internal/Python Script.py
@@ -0,0 +1,6 @@
+# Copyright (c) ${YEAR} Datanomix Inc
+#
+#  All rights reserved.  This software may not be copied, disclosed
+#  transferred, or used except in accordance with a license granted
+#  by Datanomix Inc. This software embodies proprietary
+#  information and trade secrets of Datanomix Inc.
